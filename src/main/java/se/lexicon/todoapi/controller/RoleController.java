19b8703771e0380c953ae1fd1a20b87a9c31367e
Hamed Karimi <hmdkrm.rtj@gmail.com>
2022-08-20 17:36:30 +0200

    Add @getMapping for find by id  Role Controller

--- src/main/java/se/lexicon/todoapi/controller/RoleController.java
+++ src/main/java/se/lexicon/todoapi/controller/RoleController.java
@@ -3,12 +3,15 @@ package se.lexicon.todoapi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.lexicon.todoapi.model.entity.Role;
 import se.lexicon.todoapi.repository.RoleRepository;
 
+import javax.validation.constraints.Null;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/role")
@@ -29,5 +32,17 @@ public class RoleController {
         return ResponseEntity.ok(roleList);
 
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Role> findRoleById (@PathVariable("id") Integer id){
+        System.out.println("id =" + id);
+        Role role = roleRepository.findById(id).orElse(null);
+        return ResponseEntity.ok(role);
+
+
+    }
+
+
+
 }
 
