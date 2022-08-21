e59ac8759af1888daabbd0b18d4ac9523e73a54b
Hamed Karimi <hmdkrm.rtj@gmail.com>
2022-08-21 17:13:48 +0200

    add model mapper dependency to convert entity to dto and vise-versa

--- src/main/java/se/lexicon/todoapi/controller/RoleController.java
+++ src/main/java/se/lexicon/todoapi/controller/RoleController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import se.lexicon.todoapi.model.entity.Role;
-import se.lexicon.todoapi.repository.RoleRepository;
+import se.lexicon.todoapi.exception.ObjectDuplicateException;
+import se.lexicon.todoapi.exception.ObjectNotFoundException;
+import se.lexicon.todoapi.model.dto.RoleDto;
+import se.lexicon.todoapi.service.RoleService;
 
 import javax.validation.constraints.Null;
 import java.util.List;
@@ -15,49 +17,50 @@ import java.util.Optional;
 @RequestMapping("api/v1/role")
 public class RoleController {
 
-    RoleRepository roleRepository;
+    RoleService rolesService;
 
     @Autowired
-    public RoleController(RoleRepository roleRepository) {
-        this.roleRepository = roleRepository;
+    public RoleController(RoleService rolesService) {
+        this.rolesService = rolesService;
     }
 
+
     @GetMapping("/")
-    public ResponseEntity<List<Role>> getAllRoles(){
+    public ResponseEntity<List<RoleDto>> getAllRoles(){
         System.out.println("### getAllRoles has been executed!");
-      List<Role> roleList = roleRepository.findAll();
-       // return ResponseEntity.status(HttpStatus.OK).body(roleList);
-        return ResponseEntity.ok(roleList);
+        List<RoleDto> roleDtoList = rolesService.getAll();
+       // return ResponseEntity.status(HttpStatus.OK).body(roleDtoList);
+        return ResponseEntity.ok(roleDtoList);
 
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Role> findRoleById (@PathVariable("id") Integer id){
+    public ResponseEntity<RoleDto> findRoleById (@PathVariable("id") Integer id) throws ObjectNotFoundException {
         System.out.println("id =" + id);
-        Role role = roleRepository.findById(id).orElse(null);
-        return ResponseEntity.ok(role);
+        RoleDto roleDto = rolesService.findById(id);
+        return ResponseEntity.ok(roleDto);
 
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteRoleById(@PathVariable("id") Integer id) {
+    public ResponseEntity<Void> deleteRoleById(@PathVariable("id") Integer id) throws ObjectNotFoundException {
         System.out.println("id=" + id);
-        roleRepository.deleteById(id);
+        rolesService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/")
-    public ResponseEntity<Role> createRole(@RequestBody Role role){
-        System.out.println("role=" + role);
-        Role createRole = roleRepository.save(role);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createRole);
+    public ResponseEntity<RoleDto> createRole(@RequestBody RoleDto roleDto) throws ObjectNotFoundException, ObjectDuplicateException {
+        System.out.println("roleDto=" + roleDto);
+        RoleDto createdRole = rolesService.create(roleDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdRole);
 
     }
 
     @PutMapping("/")
-    public ResponseEntity<Void> updateRoleById(@RequestBody Role role){
-        System.out.println("role=" + role);
-        roleRepository.save(role);
+    public ResponseEntity<Void> updateRoleById(@RequestBody RoleDto roleDto) throws ObjectNotFoundException {
+        System.out.println("roleDto=" + roleDto);
+        rolesService.update(new RoleDto());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
